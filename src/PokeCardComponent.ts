bad7a2e74db952bfd0e070f21ccf63f26332d1ed
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-09-28 20:01:08 -0300

    melhora o css

--- src/PokeCardComponent.ts
+++ src/PokeCardComponent.ts
@@ -26,7 +26,7 @@ export default class PokeCard extends HTMLElement{
         this.#shadow.innerHTML = 
         `
             <style>
-            
+
                 #container{
                     display: flex;
                     justify-content:center;
@@ -34,11 +34,16 @@ export default class PokeCard extends HTMLElement{
                     flex-direction: column;
                     width:auto ;
                     min-width: 180px;
-                    height:auto;
+                    z-index: 3;
+                    
+                    height:100%;
                     border:5px solid black;
+                    background-color: #e5e5f7;
+               
                     border-radius: 10px;
                     margin: 10px;
-                    box-shadow: 5px 5px 5px gainsboro;
+                    
+
                 } 
                 #background{
                     display: flex;
@@ -47,19 +52,55 @@ export default class PokeCard extends HTMLElement{
                     flex-direction: column;
                     width:auto ;
                     min-width: 180px;
-
+                    padding-top: 10px;
+                    padding-bottom: 10px;
                     background-color:whitesmoke;
                     height:auto;
                     border:5px solid black;
                     border-radius: 10px;
                     margin: 10px;
+
+                    
                 }
                 .player{
                     background-color:red
+                    background-color: #e5e5f7;
+                    
+                    background-image:  linear-gradient(135deg, #f74545 25%, transparent 25%), linear-gradient(225deg, #f74545 25%, transparent 25%), linear-gradient(45deg, #f74545 25%, transparent 25%), linear-gradient(315deg, #f74545 25%, #e5e5f7 25%);
+                    background-position:  10px 0, 10px 0, 0 0, 0 0;
+                    background-size: 10px 10px;
+                    background-repeat: repeat;
+
+
                 }
                 .enemy{
                     background-color:blue
+                    background-color: #e5e5f7;
+                    
+                    background-image:  linear-gradient(135deg, #4599f7 25%, transparent 25%), linear-gradient(225deg, #4599f7 25%, transparent 25%), linear-gradient(45deg, #4599f7 25%, transparent 25%), linear-gradient(315deg, #4599f7 25%, #e5e5f7 25%);
+                    background-position:  10px 0, 10px 0, 0 0, 0 0;
+                    background-size: 10px 10px;
+                    background-repeat: repeat;
+                }
+                img{
+                    width: 100px;
+                    border-radius: 50%;
+                    border: 5px solid black;
+                    height: 100px;
+                }
+                h2{
+                    padding: 0;
+                    margin: 0;
 
+                    background-color: gray;
+                    padding-right: 10px;
+                    padding-left: 10px;
+                    border-radius: 10px;
+
+                }
+                h1{
+                    padding: 0;
+                    margin: 0;
                 }
             </style> 
             <div id="container">
